e27c068d16ebc6d2761c8a9bb75cd83c112ccccf
Pathipat Mattra <pathipat.m@kkumail.com>
2025-04-02 16:59:16 +0700

    update learning about go

---
+++ GoAPI-Essential/fiber-test/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// Book struct to hold book data
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
+
+func main() {
+	app := fiber.New()
+
+	books = append(books, Book{ID: 1, Title: "Pathipat", Author: "Palmy"})
+	books = append(books, Book{ID: 2, Title: "MM", Author: "Meme"})
+
+	app.Get("/books", getBooks)
+	app.Get("/books/:id", getBook)
+	app.Post("/books", createBook)
+	app.Put("/books/:id", updateBook)
+	app.Delete("/books/:id", deleteBook)
+
+	app.Post("/upload", uploadFile)
+
+	app.Listen(":8080")
+
+}
+
+func uploadFile(c *fiber.Ctx) error {
+	file, err := c.FormFile("image")
+
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+	}
+
+	err = c.SaveFile(file, "./uploads"+file.Filename)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.SendString("File upload complete!")
+}
