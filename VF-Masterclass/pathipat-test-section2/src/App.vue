57ae9070e87d817a73f2243b07cc3933c9bf6d4a
Pathipat Mattra <pathipat.m@kkumail.com>
2025-04-10 17:21:57 +0700

    Add classwork Vue

---
+++ VF-Masterclass/pathipat-test-section2/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref, reactive, watchEffect} from 'vue'
+
+const formData = reactive({
+  firstname: '',
+  lastname: ''
+})
+const fullname = ref('')
+
+watchEffect(() => {
+    console.log('formData', formData);
+    fullname.value = `${formData.firstname} ${formData.lastname}`
+  }
+)
+
+</script>
+
+<template>
+  <div>
+    <div>Fullname: {{ fullname }}</div>
+    <input type="text" v-model="formData.firstname">
+    <input type="text" v-model="formData.lastname">
+  </div>
+</template>
---
+++ VF-Masterclass/pathipat-test/src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { reactive } from 'vue'
+
+  const gendersList = ['man', 'woman', 'No comment']
+  const interestsList = ['Book', 'Sport', 'Game', 'Guitar', 'Programmer']
+
+  const formData = reactive({
+    interests: []
+  })
+
+  const submitForm = () => {
+    console.log('submitForm')
+    console.log('formData', formData)
+    console.log('interests', formData.interests.join())
+    
+  }
+  
+</script>
+
+<template>
+  <div>
+    <div>
+      <div>First Name</div>
+      <input type="text" name="firstname" v-model="formData.firstname">
+    </div>
+    <div>
+      <div>Last Name</div>
+      <input type="text" name="lastname" v-model="formData.lastname">
+    </div>
+    <div>
+      <div>Gender</div>
+      <div v-for="gender in gendersList">
+        <input type="radio" name="gender" :value="gender" v-model="formData.gender">
+        {{ gender }}
+      </div>
+    </div>
+    <div>
+      <div>interests</div>
+      <div v-for="interest in interestsList">
+        <input type="checkbox" name="interest" :value="interest" v-model="formData.interests">
+        {{ interest }}
+      </div>
+    </div>
+    <div>
+      <div>Description</div>
+      <textarea v-model="formData.description"></textarea>
+    </div>
+    <button @click="submitForm()">Send Form</button>
+    <pre>{{ formData }}</pre>
+  </div>
+
+</template>
